eec21b21f25a60424614ed7568e8d976fa50dbfe
Marina <marina@marinavilardell.cat>
2024-12-23 15:25:20 +0100

    add chr name command

--- chip-seq/bedtools.sh
+++ chip-seq/bedtools.sh
@@ -3,6 +3,8 @@
 # Load module
 module load bedtools2-2.30.0-gcc-11.2.0-q7z4zez
 
+sed 's/^[^c]/chr&/' macs2/wt_sample2_peaks.narrowPeak > macs2/wt_sample2_peaks_fixed.narrowPeak
+
 # Run bedtools command
 bedtools intersect -v -a macs2/wt_sample2_fixed_peaks.narrowPeak -b ../reference_genome/mm10-blacklist.v2.bed > macs2/wt_sample2_peaks_filtered.bed
 
